2aec13d85e1864f77d826bade3e57fed2dcafc5a
pme22John <lexusls502@gmail.com>
2021-05-17 15:06:17 +0800

    上傳作業

---
+++ 許智湧_hw1.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+cv2.namedWindow("hw1", 1)
+img = cv2.imread("d:\\pme22_project\\opencv-Training\\hw1\\Bird.tif")
+c = 255 / (np.arctan(np.max(img)-128)/32)
+transformed = c * np.arctan((img-128)/32)
+transformed = np.array(transformed, dtype=np.uint8)
+cv2.imwrite("hw_1_tranformed.jpg", transformed)
+cv2.imshow("hw1", transformed)
+cv2.waitKey()
+cv2.destroyAllWindows()
